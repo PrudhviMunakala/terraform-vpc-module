fe47ce983cba8dfcb6996a44190d7ff9e9e7dc45
msrtgit <msrt10.com@gmail.com>
2026-03-22 08:39:21 +0530

    vpc

--- main.tf
+++ main.tf
@@ -157,7 +157,7 @@ resource "aws_route" "database" {
 resource "aws_route_table_association" "public" {
   count = length(var.public_subnet_cidr)
   subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.private_rt.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 # private route table association
