f6a12db1f1f2229a252b0366d1e8a3fbd6b50c97
msrtgit <msrt10.com@gmail.com>
2026-03-23 13:37:33 +0530

    repo

--- main.tf
+++ main.tf
@@ -163,7 +163,7 @@ resource "aws_route_table_association" "public" {
 # private route table association
 resource "aws_route_table_association" "private" {
   count = length(var.private_subnet_cidr)
-  subnet_id      = aws_subnet.public[count.index].id
+  subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private_rt.id
 }
 
