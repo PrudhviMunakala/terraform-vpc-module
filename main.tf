2033489cd31172b99932a6cbdf0ef609446356d1
msrtgit <msrt10.com@gmail.com>
2026-03-23 17:20:10 +0530

    pushed

--- main.tf
+++ main.tf
@@ -159,12 +159,20 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-# private route table association
+/* # private route table association
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidr)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private_rt.id
+} */
+
 resource "aws_route_table_association" "private" {
   count = length(var.private_subnet_cidr)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private_rt.id
 }
+
+
 
 # database route table association
 resource "aws_route_table_association" "database" {
